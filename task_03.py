e3ba8ceab344922988197162657ea1ec6ccfcd5b
Nick Ileczko <nickileczko@gmail.com>
2015-11-15 20:49:53 +0000

    task 03

--- task_03.py
+++ task_03.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Task 03 module"""
+"""Week 12 Synthesizing Task 03."""
+
 
 def exception_test(arg1, arg2, arg3):
+    """Except clauses may match multiple types of
+    exceptions saving unnecessary duplication and effort.
+    Args:
+        arg1, arg2, arg3.
+    Attributes:
+        caught(bool): Error if true.
+    Returns:
+        error
+    Example:
+        >>> exception_test(['apple'], 0,'p')
+        False
+        >>> exception_test(43, 1, 1)
+        True
+    """
     caught = False
     try:
         arg1[arg2].index(arg3)
-    except:
+    except(TypeError, LookupError):
         caught = True
 
     return caught
-    
